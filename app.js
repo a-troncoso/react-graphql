3f5879f6de6381d698045abaeda5e648ba7030d5
Alvaro Troncoso <alvaro.mc2@gmail.com>
2019-05-19 17:51:46 -0400

    added model and methods to execute CRUD operations

--- app.js
+++ app.js
@@ -9,6 +9,9 @@ var usersRouter = require('./routes/users');
 
 var mongoose = require('mongoose');
 var app = express();
+var graphqlHTTP = require('express-graphql');
+var schema = require('./graphql/bookSchemas');
+var cors = require('cors');
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -20,8 +23,14 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.use('*', cors());
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/graphql', cors(), graphqlHTTP({
+  schema: schema,
+  rootValue: global,
+  graphiql: true
+}))
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
